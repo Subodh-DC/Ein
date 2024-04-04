2cf36b95331cbe56b146448abae4653b16a33c74
Subodh Kumar <Subodhkumar93546270@gmail.com>
2024-04-04 14:26:28 +0530

    home page work

--- app/src/main/java/com/example/test/activity/MainActivity.kt
+++ app/src/main/java/com/example/test/activity/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.example.test.activity
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.test.modelclass.Model_for_image
 import com.example.test.R
 
@@ -30,7 +34,17 @@ class MainActivity : AppCompatActivity() {
         val button3=findViewById<Button>(R.id.himageButton3)
         val imageView=findViewById<ImageView>(R.id.himage)
         val quotes=findViewById<TextView>(R.id.quotes)
+        val hamburger=findViewById<ImageView>(R.id.hamburger_icon)
+        val notification=findViewById<ImageView>(R.id.notification_bell)
+        val drawerlayout=findViewById<DrawerLayout>(R.id.drawer)
 
+        hamburger?.setOnClickListener {
+            drawerlayout?.openDrawer(GravityCompat.START)
+        }
+
+        notification.setOnClickListener {
+            Toast.makeText(this, "Notification", Toast.LENGTH_SHORT).show()
+        }
 
         button.setOnClickListener {
             val intent=Intent(this, MainActivity2::class.java)
@@ -58,8 +72,19 @@ class MainActivity : AppCompatActivity() {
 
         val imageDetailsList = listOf(
             Model_for_image(R.drawable.lalkila, "Description 1","\"Make each day your masterpiece\""),
-            Model_for_image(R.drawable.indiagate, "Description 2","\"test2\""),
-            Model_for_image(R.drawable.tajmahel, "Description 3", "\"test3\"")
+            Model_for_image(R.drawable.indiagate, "Description 2","\"Nothing is impossible. The word itself says 'I'm possible!\""),
+            Model_for_image(R.drawable.tajmahel, "Description 3", "\"The bad news is time flies. The good news is you're the pilot.\""),
+            Model_for_image(R.drawable.earth_removebg, "Description 3", "\"Spread love everywhere you go.\""),
+            Model_for_image(R.drawable.indiaflag_removebg, "Description 3", "\"Some people look for a beautiful place. Others make a place beautiful.\""),
+            Model_for_image(R.drawable.map_removebg, "Description 3", "\"Happiness is not by chance, but by choice.\""),
+            Model_for_image(R.drawable.math_removebg, "Description 3", "\"You cannot change what you are, only what you do.\""),
+            Model_for_image(R.drawable.moutain_removebg, "Description 3", "\"I have not failed. I've just found 10,000 ways that won't work.\""),
+            Model_for_image(R.drawable.robot_removebg, "Description 3", "\"I've got nothing to do today but smile.\""),
+            Model_for_image(R.drawable.seterlight_removebg, "Description 3", "\"\""),
+            Model_for_image(R.drawable.tree_removebg, "Description 3", "\"\""),
+            Model_for_image(R.drawable.chemistry_removebg, "Description 3", "\"\""),
+            Model_for_image(R.drawable.dna_removebg, "Description 3", "\"\""),
+            Model_for_image(R.drawable.download18569, "Description 3", "\"\"")
 
         )
 
